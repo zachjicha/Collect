f83461ed667e3c7d4b4b07568b3ec056ea76f9e9
harshkaria <harsh.karia1@gmail.com>
2019-07-07 17:32:10 -0700

    commit api2

--- CollectApp/CollectApp/ViewController.swift
+++ CollectApp/CollectApp/ViewController.swift
@@ -11,6 +11,11 @@ import Alamofire
 
 class ViewController: UIViewController {
     // API Key: a31246109d0211e98bfadfb7eb1aa8b5
+    struct Item : Codable
+    {
+        var totalAmount : Int
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,15 +35,25 @@ class ViewController: UIViewController {
         
         let img = UIImage(named: "rec.jpg")
         guard let img_data = img?.jpegData(compressionQuality: 1.0) else { return  }
+        var savedData : Data
         
         // Use Alamofire to upload image
         AF.upload(multipartFormData: { (multipartFormData) in
             multipartFormData.append(img_data, withName: "file", fileName: "rec.img", mimeType: "image/jpg")
         }, to: url, method: .post, headers: headers)
-            .responseJSON { (data) in
-                print(data)
+            .responseJSON { (response) in
+                print(response)
+                switch response.result
+                {
+                case .success(let data):
+                    guard let json = data as? [String : AnyObject] else { return }
+                    var amount = json["totalAmount"]?["data"] as! Double
+                    print(amount)
+                case .failure(let error):
+                    print(error)
+                }
+                
         }
-        
 }
 
 }
