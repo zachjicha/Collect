9ff0637482f1dbb25c0b3e1ff2fcad579d2049af
harshkaria <harsh.karia1@gmail.com>
2019-07-20 21:39:33 -0700

    UI changes

--- Collect/Collect/ReceiptsViewController.swift
+++ Collect/Collect/ReceiptsViewController.swift
@@ -8,10 +8,204 @@
 
 import UIKit
 import CoreData
-
-class ReceiptsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+import SCLAlertView
+import Alamofire
+import SwiftyJSON
+class ReceiptsViewController: UIViewController, UITableViewDelegate, UIImagePickerControllerDelegate, UITableViewDataSource, UINavigationControllerDelegate {
+    var selectedPhoto:UIImage!
+    var Items : [Item] = []
+    // Global Receipt Name
+    var recName : String = ""
     
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func newReceiptButton(_ sender: Any)
+    {
+        // Custom alert view
+        let alert = SCLAlertView()
+        let GetInfo = alert.addTextField("Enter receipt name")
+        alert.addButton("Add Receipt") {
+            // If field is empty
+            if (GetInfo.text == "") {
+                print("Please Enter a receipt Name")
+                SCLAlertView().showError("Add Error", subTitle: "You must enter a receipt name")
+                return
+            }
+            if (CheckDuplicity(receiptName: GetInfo.text!) == true) {
+                print("Data already exists")
+                SCLAlertView().showError("Add Error", subTitle: "This receipt name already exists.")
+                return
+            }
+            else
+            {
+                self.recName = GetInfo.text!
+
+                //Image picker controller picks an image from either the
+                //camera or camera roll based on user selection
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.delegate = self
+                
+                //Action sheet to pick photo source
+                let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+                
+                //Add source slections to action sheet
+                actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+                    
+                    //Check if camera is available before accessing it
+                    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                        imagePickerController.sourceType = .camera
+                        self.present(imagePickerController, animated: true, completion: nil)
+                    }
+                    else {
+                        
+                        //Alert the user that the camera is not available
+                        //This will probably never happen
+                        let noCameraAlert = UIAlertController(title: "Camera Not Available", message: "The device's camera cannot be accessed by Collect", preferredStyle: .alert)
+                        noCameraAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(noCameraAlert, animated: true, completion: nil)
+                        
+                    }
+                    
+                } ))
+                
+                actionSheet.addAction(UIAlertAction(title: "Camera Roll", style: .default, handler: { (action: UIAlertAction) in
+                    
+                    imagePickerController.sourceType = .photoLibrary
+                    self.present(imagePickerController, animated: true, completion: nil)
+                } ))
+                
+                actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                
+                //Present the action sheet
+                self.present(actionSheet, animated: true, completion: nil)
+                
+            }
+        }
+        alert.showEdit("Add a Receipt", subTitle: "Either upload from camera roll or take a picture")
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //Get the image from the picker controller and set it to the selected photo
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        selectedPhoto = image
+    
+        
+        //Dismiss the picker controller
+        picker.dismiss(animated: true, completion: self.parseReceipt)
+        
+        
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //Parse the receipt using Harsh's code
+    func parseReceipt() {
+        
+        print("IN API CALL")
+        // Hide close button
+        let appearance = SCLAlertView.SCLAppearance(
+            showCloseButton: false
+        )
+        
+        let alert = SCLAlertView(appearance: appearance).showWait(self.recName, subTitle: "Adding receipt to Collect...", closeButtonTitle: nil, timeout: nil, colorStyle: nil, colorTextButton: 0xFFFFFF, circleIconImage: nil, animationStyle: SCLAnimationStyle.topToBottom)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            alert.setSubTitle("Progress: 50%")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                alert.setSubTitle("Progress: 80%")
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    alert.setSubTitle("Progress: 95%")
+                    
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    alert.setSubTitle("Progress: 100%")
+                    alert.close()
+                }
+            }
+        }
+        
+        
+        // API Setup:
+        let key = "a31246109d0211e98bfadfb7eb1aa8b5" // API-Key
+        guard let url = URL(string: "https://api-au.taggun.io/api/receipt/v1/verbose/file") else {
+            return
+        } // URL for API
+        
+        // Headers for JSON request
+        let headers: HTTPHeaders = [
+            "apikey": key,
+            "Content-type": "multipart/form-data",
+            "Accept" : "application/json"]
+        
+        guard let img_data = selectedPhoto.jpegData(compressionQuality: 1.0) else { return  }
+        // Use Alamofire to upload image
+        AF.upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(img_data, withName: "file", fileName: "rec.img", mimeType: "image/jpg")
+        }, to: url, method: .post, headers: headers)
+            .responseJSON { (response) in
+                switch response.result
+                {
+                case .success(let value):
+                    let json = JSON(value)
+                    //print(json["amounts"])
+                    //print(json)
+                    
+                    //Gets the total amount and the tax amount
+                    let totalAmount = json["totalAmount"]["data"].double
+                    let taxAmount = json["taxAmount"]["data"].double
+                    
+                    //Gets the tax percent of the receipt based on totalAmount and taxAmount
+                    let taxPercent = Double(taxAmount!)/Double(totalAmount!)
+                    
+                    //print("TOTAL AMOUNT: \(String(describing: totalAmount))")
+                    //print("TAX AMOUNT: \(String(describing: taxAmount))")
+                    if let amounts = json["amounts"].array
+                        
+                    {
+                        for JSONItem in amounts
+                        {
+                            let item = Item(itemName: JSONItem["text"].string, totalAmount: JSONItem["data"].double)
+                            // print(item.itemName!, item.totalAmount!)
+                            let lowerItem = item.itemName!.lowercased()
+                            if(!lowerItem.contains("total") && !lowerItem.contains("debit") && !lowerItem.contains("debit")) {
+                                self.Items.append(item)
+                            }
+                        }
+                        //self.printItems()
+                       // self.loadingText.text = "Items:"
+                        
+                        print("Attempting to Print!")
+                        
+                        
+                        for item in self.Items
+                        {
+                            //self.outputText.text += item.itemName!
+                            //self.outputText.text += "\n"
+                            
+                            print(item.itemName!)
+                        }
+                        
+                        
+                        //Saves item content to local storage
+                        //Passes array of Items
+                        SaveAllReceiptData(NameOfReceipt: self.recName, Items: self.Items, taxPercent: taxPercent)
+                        self.viewDidAppear(false)
+                    }
+                    
+                case .failure(let error):
+                    print(error)
+                }
+        }
+    }
+    
+    
     var AllReceipts:[Receipt] = []  //Array of receipts
     var receiptToOpen = ""
     
@@ -89,5 +283,10 @@ class ReceiptsViewController: UIViewController, UITableViewDelegate, UITableView
         }
         AllReceipts = receiptObj
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.FetchData()
+        self.tableView.reloadData()
     }
 }
--- Collect/Pods/Target Support Files/Pods-Collect/Pods-Collect-frameworks.sh
+++ Collect/Pods/Target Support Files/Pods-Collect/Pods-Collect-frameworks.sh
@@ -162,11 +162,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SCLAlertView/SCLAlertView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyGif/SwiftyGif.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SCLAlertView/SCLAlertView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyGif/SwiftyGif.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
