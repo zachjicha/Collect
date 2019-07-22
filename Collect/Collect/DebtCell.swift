863af086cabce3532dcefbdde33a4342a3a673a2
Norris <channorris98@gmail.com>
2019-07-22 14:42:34 -0700

    Added files DebtCell.swift and DebtListViewController.swift

---
+++ Collect/Collect/DebtCell.swift
@@ -0,0 +1,29 @@
+//
+//  DebtCell.swift
+//  Collect
+//
+//  Created by Norris Chan on 7/21/19.
+//  Copyright © 2019 The Collective. All rights reserved.
+//
+
+import UIKit
+
+class DebtCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var switchToggle: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+
+}
