3768b238327db648819d88ac4a1eea88f8d6292e
harshkaria <harsh.karia1@gmail.com>
2020-03-05 21:44:13 -0800

    commit codeine people list change

---
+++ Collect/PeoplePaying+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PeoplePaying+CoreDataClass.swift
+//  
+//
+//  Created by Harsh Karia on 3/5/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PeoplePaying)
+public class PeoplePaying: NSManagedObject {
+
+}
